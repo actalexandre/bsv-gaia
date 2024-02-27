28dab6f14734b076f4f705ab7f1b09d3d7e4bf25
Alexandre <alexandre@acta-ds.fr>
2024-02-27 15:22:03 +0100

    feat: improve llm prompt input

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -13,30 +13,40 @@ import { AutoLinkNode, LinkNode } from '@lexical/link';
 import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
 import { ListPlugin } from '@lexical/react/LexicalListPlugin';
 import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
-import { TRANSFORMERS } from '@lexical/markdown';
 import ListMaxIndentLevelPlugin from './plugins/ListMaxIndentLevelPlugin';
 import AutoLinkPlugin from './plugins/AutoLinkPlugin';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import {
-  $getSelection,
   $createParagraphNode,
   $getRoot,
   $createLineBreakNode,
-  TextNode,
 } from 'lexical';
 import { client } from '@gradio/client';
 import { useState } from 'react';
-import React from 'react';
 import DraggableBlockPlugin from './plugins/DraggableBlockPlugin';
 import ToolbarPlugin from './plugins/ToolbarPlugin';
 import FloatingTextFormatToolbarPlugin from './plugins/FloatingTextFormatToolbarPlugin';
-import { IconButton, TextField } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  CircularProgress,
+  IconButton,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Typography,
+} from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import { $getNodeByKey, $createTextNode } from 'lexical';
 import InlineImagePlugin from './plugins/InlineImagePlugin';
 import { InlineImageNode } from './nodes/InlineImageNode';
 import ImagesPlugin from './plugins/ImagesPlugin';
 import { ImageNode } from './nodes/ImageNode';
+import {
+  $convertFromMarkdownString,
+  $convertToMarkdownString,
+  TRANSFORMERS,
+} from '@lexical/markdown';
 
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
@@ -64,7 +74,7 @@ const editorConfig = {
     AutoLinkNode,
     LinkNode,
     InlineImageNode,
-    ImageNode
+    ImageNode,
   ],
 };
 
@@ -118,61 +128,91 @@ function AIPromptInput() {
   const [editor] = useLexicalComposerContext();
 
   const [userInput, setUserInput] = useState('');
+  const [loading, setLoading] = useState(false);
 
+  const fillInput = (text: string) => {
+    setUserInput(text)
+  }
   const askAI = async () => {
-    const app = await client(import.meta.env.VITE_APP_GRADIO_CLIENT_URL, {});
-    const job = app.submit('/chat', [
-      userInput,
-      'unused', // not used but required for Gradio API
-    ]);
-    setUserInput('');
+    try {
+      setLoading(true);
+      const allText = editor.getRootElement()?.textContent;
+      setUserInput('');
+      const app = await client(import.meta.env.VITE_APP_GRADIO_CLIENT_URL, {});
+      const res = await app.predict('/chat', [
+        userInput,
+        allText, // not used but required for Gradio API
+      ]);
 
-    editor.update(() => {
-      let root = $getRoot();
-      const paragraph = $createParagraphNode();
-      root.append(paragraph);
-      const textNode = $createTextNode();
-      paragraph.append(textNode);
-    });
-
-    job.on('data', (payload) => {
       editor.update(() => {
-        const root = $getRoot();
-        root.selectEnd();
-        const selection = $getSelection();
-        selection?.insertText(payload.data[0]);
+        let root = $getRoot();
+        const paragraph = $createParagraphNode();
+        const lb = $createLineBreakNode();
+        const emptyTextNode = $createTextNode('\n\n');
+        paragraph.append(lb);
+        paragraph.append(emptyTextNode);
+        root.append(paragraph);
+        const previousMd = $convertToMarkdownString(TRANSFORMERS);
+        $convertFromMarkdownString(
+          previousMd + '\n\n' + res.data[0],
+          TRANSFORMERS
+        );
       });
-    });
-
-    job.on('status', (status) => {
-      if (status === 'completed') {
-        // do something on completion
-      }
-    });
+    } finally {
+      setLoading(false);
+    }
   };
   return (
-    <form
-      style={{ width: '100%', display: 'flex' }}
-      onSubmit={(event) => {
-        event.preventDefault();
-        askAI();
-      }}
-    >
-      <TextField
-        style={{ backgroundColor: 'white', flex: 1 }}
-        id="outlined-multiline-flexible"
-        className="bg-white"
-        label=""
-        multiline
-        value={userInput}
-        onChange={(event) => {
-          setUserInput(event.target.value);
+    <div>
+      <form
+        style={{ width: '100%', display: 'flex' }}
+        onSubmit={(event) => {
+          event.preventDefault();
+          askAI();
         }}
-        // maxRows={40}
-      />
-      <IconButton type="submit">
-        <ArrowUpwardIcon />
-      </IconButton>
-    </form>
+      >
+        <TextField
+          style={{ backgroundColor: 'white', flex: 1 }}
+          id="outlined-multiline-flexible"
+          className="bg-white"
+          label=""
+          value={userInput}
+          onChange={(event) => {
+            setUserInput(event.target.value);
+          }}
+          // maxRows={40}
+        />
+        <IconButton type="submit">
+          {loading ? (
+            <CircularProgress></CircularProgress>
+          ) : (
+            <ArrowUpwardIcon />
+          )}
+        </IconButton>
+      </form>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="div">
+            Exemples de prompts
+          </Typography>
+          <Typography variant="body2">
+            <br></br>
+            {exemplePrompts.map((prompt) => (
+              <ListItemButton component="a" href="#simple-list" onClick={() => fillInput(prompt)}>
+                <ListItemText primary={prompt} />
+              </ListItemButton>
+            ))}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
+
+const exemplePrompts = [
+  "Rédige une analyse de la phénologie décrivant les différentes situations observées pour la culture sur le réseau Charentes",
+  "Rédige une courte synthèse sur les conditions météorologiques des 7 derniers jours ainsi que sur les prévisions météorologiques pour la semaine à venir.",
+  "Rédige un encart « biodiversité » sur les auxiliaires de culture de la vigne présent sur la période en cours (bénéfices, ravageurs ciblés et conseils pratiques).",
+  "Rédige un encart “mémo de l’Observateur” qui fait un résumé des bonnes pratiques d’observations aux champs et suggère la recherche de premiers symptômes pour les bioagresseurs à forte pression épidémiologique sur la période en cours.",
+  "A partir du bulletin de santé végétal, rédige une rubrique de synthèse intitulé “Ce qu’il faut retenir”, comprenant un récapitulatif de deux lignes maximum sur les éléments essentiels à retenir."
+]
